1c956f0db7f0d6f928e5e814e887a04c7da0e62a
regycloud <regycloud@yahoo.com>
2022-07-21 23:07:17 +0700

    fix v2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,6 +13,10 @@ const NavBar = () => {
         await keluarDariAplikasi();
         navigate("/login");
     };
+    const buttonLoginOnClickHandler = async () => {
+        navigate("/login");
+    };
+
     const [user] = useAuthState(auth)
 
     return (
@@ -25,10 +29,14 @@ const NavBar = () => {
                     <Typography variant="h5" sx={{}}>Profile</Typography>
                     <Typography variant="h6" className={styles.grow} sx={{textDecoration: 'none'}}>
                    </Typography>
-                    <Typography>Hallo {user?.email}</Typography>                    
-                    <Button color="inherit" onClick={buttonLogoutOnClickHandler}>
-                        Log Out
-                    </Button>
+                   {user !== null? <Typography>Hallo {user?.email} </Typography> : ''}
+                    {user !== null ? <Button color="inherit" onClick={buttonLogoutOnClickHandler}>
+                          Log Out
+                    </Button> : <Button color="inherit" onClick={buttonLoginOnClickHandler}>
+                        Log In
+                    </Button>}         
+
+                   
                 </Toolbar>
             </AppBar>
         </Box>
