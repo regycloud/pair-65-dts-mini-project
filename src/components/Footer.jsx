25ff44495d2dbd56969b433232f6aba6354d5329
regycloud <regycloud@yahoo.com>
2022-07-22 20:25:21 +0700

    Edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
         <Box sx={{background: '#121212', borderColor: 'white', margin: 0, height: '8vh'}}>
-            <Typography sx={{textAlign: 'center', alignContent: 'center', paddingTop:2, color: 'whitesmoke'}}> 2022 Movies Pair-65
+            <Typography sx={{textAlign: 'center', alignContent: 'center', paddingTop:2, color: 'whitesmoke'}}> 2022 Movies Pair-65-dts project
             </Typography>
         </Box>
     </>
